04b77981ee8abb4b8b13266bcf3b897271b3fc25
Vitaly <lub-olen@yandex.ru>
2021-12-05 08:26:56 +0300

    Initial Commit

---
+++ HomeWork1.1/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  HomeWork1.1
+//
+//  Created by Vitally Ochnev on 05.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func changeLayout(_ sender: UIButton) {
+        
+        sender.tag += 1
+        if sender.tag > 7 { sender.tag = 0
+        }
+        
+        switch sender.tag {
+        case 1:
+            view.backgroundColor = .red
+        case 2:
+            view.backgroundColor = .orange
+        case 3:
+            view.backgroundColor = .yellow
+        case 4:
+            view.backgroundColor = .green
+        case 5:
+            view.backgroundColor = .cyan
+        case 6:
+            view.backgroundColor = .blue
+        case 7:
+            view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .black
+    
+        }
+    }
+}
